4d28fe35b1a41249ff411b3db17c755f535c0f1b
johanalauk <johanalauk96@hotmail.com>
2017-07-28 14:09:32 -0300

    Alarma

--- Dseda/src/Controlador/ProductoDAO.java
+++ Dseda/src/Controlador/ProductoDAO.java
@@ -131,7 +131,7 @@ public class ProductoDAO {
         }
         catch(Exception e)
         {
-            JOptionPane.showMessageDialog(null, "Error");
+            JOptionPane.showMessageDialog(null, "Error. Listar productos");
         }
         return lista;
     }
@@ -145,18 +145,37 @@ public class ProductoDAO {
             Session session = sesion.openSession();
             Transaction tx = session.beginTransaction();
             Query query = session.createQuery("FROM Producto p WHERE p.codigo LIKE :codigo");
-            query.setParameter("codigo", "%"+codigo+"%");
+            query.setParameter("codigo", codigo+"%");
             lista = query.list();
             tx.commit();
         }
         catch(Exception e)
         {
-            JOptionPane.showMessageDialog(null, "Error");
+            JOptionPane.showMessageDialog(null, "Error. Producto por codigo");
         }
         return lista;
     }
     
-   /*
+    public List<Producto> alarma()
+    {
+        List<Producto> lista = null;
+        try
+        {
+            SessionFactory sesion = NewHibernateUtil.getSessionFactory();
+            Session session = sesion.openSession();
+            Transaction tx = session.beginTransaction();
+            Query query = session.createQuery("FROM Producto p WHERE p.stock <= p.stockMinimo");
+            lista = query.list();
+            tx.commit();
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, "Error. Lista stock minimo");
+        }
+        return lista;
+    }
+    
+   
     public List<Producto> buscarPorCodigoNombre(String cadena)
     {
         List<Producto> lista = null;
@@ -172,9 +191,9 @@ public class ProductoDAO {
         }
         catch(Exception e)
         {
-            JOptionPane.showMessageDialog(null, "Error");
+            JOptionPane.showMessageDialog(null, "Error. Producto por codigo o descripcion");
         }
         return lista;
     }
-    */
+    
 }
